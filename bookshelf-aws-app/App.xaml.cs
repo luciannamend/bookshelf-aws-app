0e38c75e15be5cd98c549bd196e85d4ce6dcbc42
Lucianna Mendonca <luciannamend@gmail.com>
2024-09-27 19:51:06 -0400

    Add project files.

---
+++ bookshelf-aws-app/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace bookshelf_aws_app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
