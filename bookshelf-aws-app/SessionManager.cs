9e8cb508d4b78af285d7f63c299a6d339f6fc052
Lucianna Mendonca <luciannamend@gmail.com>
2024-10-04 15:35:04 -0400

    Created a singleton pattern to keep user info: user and bookshelf

---
+++ bookshelf-aws-app/SessionManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookshelf_aws_app
+{
+    public class SessionManager
+    {
+        private static SessionManager _instance;
+
+        // Properties to hold the current user and their bookshelf
+        public User CurrentUser { get; set; }
+        public Bookshelf CurrentBookshelf { get; set; }
+
+        // Private constructor to prevent instantiation from other classes
+        private SessionManager() { }
+
+        // Method to get the single instance of the SessionManager class
+        public static SessionManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SessionManager();
+                }
+                return _instance;
+            }
+        }
+    }
+}
